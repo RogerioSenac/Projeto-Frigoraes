92ecd5b178acba2cd1f7c3db61d176a96556cc4f
rogeriosenac <roge.pupo.toledo@gmail.com>
2024-11-29 16:30:56 -0300

    initial projeto index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<?php
+include($server['DOCUMENT_ROOT']. "/Projetos_GIT/Projeto-Frigoraes/includes/header.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="./assets/css/estilo.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"> <!--link para mapa -->
     <title>Frigoraes Alimentos</title>
 </head>
 <body>
-    
+    <section id="carrocel">
+
+    </section>
+
+    <section>
+        
+    </section>
 </body>
 </html>
